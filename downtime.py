0e0ea37816c6c412540be360a7e98fc97108bd49
Ahoora <ahoura.78@gmail.com>
2025-07-14 12:12:29 -0400

    Created test.py

--- downtime.py
+++ downtime.py
@@ -2,18 +2,21 @@ import csv, requests
 
 
 WEBSITES = [
-    {"name": "google", "url": "https://www.google.com"},
-    {"name": "facebook", "url": "https://www.facebook.com"},
-    {"name": "twitter", "url": "https://www.twitter.com"},
-    {"name": "github", "url": "https://www.github.com"},
-    {"name": "linkedin", "url": "https://www.linkedin.com"},
-    {"name": "reddit", "url": "https://www.reddit.com"},
+    {"name": "Google", "url": "https://www.google.com"},
+    {"name": "Facebook", "url": "https://www.facebook.com"},
+    {"name": "Twitter", "url": "https://www.twitter.com"},
+    {"name": "Github", "url": "https://www.github.com"},
+    {"name": "Linkedin", "url": "https://www.linkedin.com"},
+    {"name": "Reddit", "url": "https://www.reddit.com"},
     {"name": "RBC", "url": "https://www.rbcroyalbank.com"},
+    {"name": "TD", "url": "https://www.rbcroyalbank.com"},
+    
 
 ]
 
 with open("websites.csv", "a") as file:
     writer = csv.DictWriter(file, fieldnames=["website_name", "url"])
+    writer.writeheader()
     for website in WEBSITES:
         writer.writerow({"website_name": website["name"], "url": website["url"]})
 
